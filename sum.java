53bb2e91ee2240ca5b045f060b8034d51912dc26
Patel-Muhammad <patelmuh2003@gmail.com>
2023-01-14 20:00:23 +0530

    This is second commit diff

--- sum.java
+++ sum.java
@@ -2,8 +2,6 @@ public class sum{
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
-
-        int c = a + b;
-        System.out.println(c);
+        System.out.println(a+b);
     }
 }
